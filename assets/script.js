f2e3f4abd3aee663ace8e134f076b12eeb927411
qualose123 <jonhyzicaos@gmail.com>
2022-04-15 14:39:57 -0300

    Protótipo de projeto v1

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+let imgForma = document.querySelector(".base");
+let nameForma = document.querySelector("#evoluir");
+let botao = document.querySelector("#transformar");
+
+let i = 0;
+
+console.dir(imgForma.setAttribute);
+
+function alterarBotao() {
+  i++;
+  if (i == 1) {
+    imgForma.setAttribute("src", "assets/img/gif/gear second.gif");
+    imgForma.setAttribute("alt", "Gear Second");
+    nameForma.innerHTML = "Gear Second";
+  } else if (i == 2) {
+    imgForma.setAttribute("src", "assets/img/gif/luffy-gear third.gif");
+    imgForma.setAttribute("alt", "Gear third");
+    nameForma.innerHTML = "Gear third";
+  } else if (i == 3) {
+    imgForma.setAttribute("src", "assets/img/gif/gear four.gif");
+    imgForma.setAttribute("alt", "Gear Four");
+    nameForma.innerHTML = "Gear Four";
+  } else if (i == 4) {
+    imgForma.setAttribute("src", "assets/img/gif/snakeman.gif");
+    imgForma.setAttribute("alt", "Gear Four snakeman");
+    nameForma.innerHTML = "Gear Four Snakeman";
+    botao.textContent="Retroceder";
+  }
+
+  if (i > 4) {
+    i = 0;
+
+    imgForma.setAttribute("src", "./assets/img/forma base.jpg");
+    imgForma.setAttribute("alt", "Forma Base");
+    nameForma.innerHTML = "Forma Base";
+    botao.textContent="Transformar";
+  }
+}
